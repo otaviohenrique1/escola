9ea51381e6a15d43a296c05df0ebe713903f651d
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-07-11 20:26:41 -0300

    Commit 1

---
+++ escola/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+def estudantes(request):
+    if request.method == "GET":
+        estudante = {
+            "id": "1",
+            "nome": "lais",
+        }
+        return JsonResponse(estudante)
