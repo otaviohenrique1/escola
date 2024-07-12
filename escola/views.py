f2e1b910c12a3f4e404efa7ac7c77a1cafc92908
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-07-12 13:48:37 -0300

    Commit 3

--- escola/views.py
+++ escola/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from escola.models import Estudante, Curso
+from escola.serializer import EstudanteSerializer, CursoSerializer
+from rest_framework import viewsets
 
 
-def estudantes(request):
-    if request.method == "GET":
-        estudante = {
-            "id": "1",
-            "nome": "lais",
-        }
-        return JsonResponse(estudante)
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
